24b171b76fd4707d668c8b2ae64a78164c0ff3e1
mwisnia97 <46759954+mwisnia97@users.noreply.github.com>
2021-07-19 10:46:11 +0200

    Photo details page (#6)
    * Hotfix
    
    * Add html wireframe
    
    * Add page wrapper
    
    * Header and footer styled
    
    * Return button styled
    
    * Image with img area styled
    
    * Detailes styled
    
    * Fix dancing button
    
    * Small image fix style
    
    * Hotfix index.js
    
    Co-authored-by: Jakub Łyczko <jakub.lyczko1@gmail.com>

--- frontend/src/assets/GlobalStyle.js
+++ frontend/src/assets/GlobalStyle.js
@@ -1,10 +1,11 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-html{
+*{
     box-sizing: border-box;
     margin: 0;
     padding: 0;
+    font-family: Poppins, Lato, San Francisco, Helvetica, sans-serif;
 }
 
 background-color: red;
