7ce325a83f8b74ef0920fe69cd52572e4357de10
Jakub Lyczko <jakub.lyczko1@gmail.com>
2021-07-21 09:23:41 +0200

    Deleted backend dir

--- frontend/src/components/UploadedImage/UploadedImage.styles.js
+++ frontend/src/components/UploadedImage/UploadedImage.styles.js
@@ -4,8 +4,6 @@ export const UploadedImage = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  max-width: 100%;
-  max-height: 100%;
   padding-top: 2%;
   border-radius: 5%;
 `;
@@ -16,15 +14,6 @@ export const Image = styled.div`
   width: 90%;
   height: 260px;
 
-  /* button {
-    padding: 5px 10px;
-    border-radius: 10%;
-    border: none;
-    position: relative;
-    left: 300px;
-    bottom: 40px;
-    cursor: pointer;
-  } */
   p {
     padding: 5px;
     text-align: center;
@@ -34,8 +23,6 @@ export const Image = styled.div`
     border-top-right-radius: 10%;
     width: 100%;
     height: 85%;
-    /* width: 70%;
-  height: 70%; */
     object-fit: cover;
   }
 `;
