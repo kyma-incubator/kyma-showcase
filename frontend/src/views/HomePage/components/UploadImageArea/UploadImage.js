9237b3624b36150a370109d0dc47a73c8ed289c8
Jakub Lyczko <jakub.lyczko1@gmail.com>
2021-07-30 11:49:28 +0200

    Basic implementation of uploading image

--- frontend/src/views/HomePage/components/UploadImageArea/UploadImage.js
+++ frontend/src/views/HomePage/components/UploadImageArea/UploadImage.js
@@ -1,10 +1,36 @@
 import { StyledUploadImage } from './UploadImage.styles';
+import { useState } from 'react';
 
 const UploadImage = () => {
+  const [base64Image, setBase64Image] = useState('');
+
+  const uploadImage = async (event) => {
+    const image = event.target.files[0];
+
+    const convertedImage = await convertImageToBase64(image);
+    setBase64Image(convertedImage);
+  };
+
+  const convertImageToBase64 = (image) => {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(image);
+
+      fileReader.onload = () => {
+        return resolve(fileReader.result);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+  };
+
   return (
     <StyledUploadImage>
       <p>Upload an image or drag and drop</p>
-      <div></div>
+      <input type="file" accept="image/*" text="Upload file" onChange={(e) => uploadImage(e)} />
+      <img src={base64Image} alt="uploaded image" />
     </StyledUploadImage>
   );
 };
