d07bb7c0fc49b112f2d1af6e5b3b90bc93cbcddd
Hanna Gosciniak <hanngos10@gmail.com>
2021-07-30 13:30:02 +0200

    error handling

--- backend/database.go
+++ backend/database.go
@@ -3,7 +3,7 @@ package main
 import (
 	"context"
 	"errors"
-	//"fmt"
+
 	"github.com/go-redis/redis/v8"
 	log "github.com/sirupsen/logrus"
 )
@@ -23,10 +23,10 @@ func NewDatabaseConnection(address, password string) Database {
 	}
 }
 
-func (d *Database) Connect() error { //niejawnie wykonac Connect (?) w innej metodzie, na razie zostawic
+func (d *Database) Connect() error {
 	if d.connection != nil {
-		//return errors.New("connection already exists")
-		return nil //ta opcja
+		log.Info("Connection to database is already established")
+		return nil
 	}
 
 	d.connection = redis.NewClient(&redis.Options{
@@ -43,7 +43,7 @@ func (d *Database) Connect() error { //niejawnie wykonac Connect (?) w innej met
 
 func (d Database) InsertToDB(key string, value string) error {
 	if d.connection == nil {
-		return errors.New("connection not initialized")
+		return errors.New("INSERTTODB: connection not initialized")
 	}
 	_, err := d.connection.Set(d.ctx, key, value, 0).Result()
 	return err
@@ -51,31 +51,30 @@ func (d Database) InsertToDB(key string, value string) error {
 
 func (d Database) GetFromDB(key string) (interface{}, error) {
 	if d.connection == nil {
-		return nil, errors.New("connection not initialized")
+		return nil, errors.New("GETFROMDB: connection not initialized")
 	}
 
-	val, err := d.connection.Get(d.ctx, key).Result() ///zwracanie errorów -> gdy jest error to nie zwraca wartości?!
+	val, err := d.connection.Get(d.ctx, key).Result()
 
 	switch {
 	case err == redis.Nil:
-		log.Printf("%s key does not exist", key)
+		err = errors.New("GETFROMDB:key " +  key + " does not exist")
 	case err != nil:
-		log.Println(err)
+		err = errors.New("GETFROMDB:error: " +  err.Error() + " occurred in getting data from db")
 	case val == "":
-		log.Println("value is empty")
+		err = errors.New("GETFROMDB:for key " +  key + " value is empty")
 	}
 	return val, err
 }
 
 func (d Database) GetAllKeys() ([]string, error) {
 	if d.connection == nil {
-		return nil, errors.New("connection not initialized")
+		return nil, errors.New("GETALLKEYS: connection not initialized")
 	}
 
 	keys, err := d.connection.Keys(d.ctx, "*").Result()
 	if err != nil {
 		return nil, err
 	}
-
 	return keys, nil
 }
