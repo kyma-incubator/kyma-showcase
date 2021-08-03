bfa8d1e21e615dfa5177b18a675e7d46af152a33
Rafał Foks <r.fok74@gmail.com>
2021-08-03 14:04:36 +0200

    Added corner cases and removed one-liner

--- backend/api_test.go
+++ backend/api_test.go
@@ -9,9 +9,10 @@ import (
 	"net/http/httptest"
 	"testing"
 )
+//dodac testy sprawdzajace wszytkie case'y
 
 func TestDBGetAllHandler(t *testing.T) {
-	t.Run("should return error when database is empty", func(t *testing.T) {
+	t.Run("should not return error when database is empty", func(t *testing.T) {
 
 		//given
 		req, err := http.NewRequest("GET", "v1/images", nil)
@@ -20,13 +21,14 @@ func TestDBGetAllHandler(t *testing.T) {
 		recorder := httptest.NewRecorder()
 		dbManagerMock := mocks.DBManager{}
 		testSubject := NewHandler(&dbManagerMock)
-		dbManagerMock.On("GetAllKeys").Return(nil, errors.New("Empty database, returned nil"))
+		dbManagerMock.On("GetAllKeys").Return(nil, nil)
 
 		//when
 		testSubject.DBGetAllHandler(recorder, req)
 
 		//then
-		assert.Equal(t, http.StatusInternalServerError, recorder.Code)
+		dbManagerMock.AssertNumberOfCalls(t, "GetAllKeys",1)
+		assert.Equal(t, http.StatusOK, recorder.Code)
 
 	})
 
@@ -48,10 +50,15 @@ func TestDBGetAllHandler(t *testing.T) {
 		//when
 		testSubject.DBGetAllHandler(recorder, req)
 
-		//then
+		//then //czy metody na mocku zostaly wywolane!!! assert
+		dbManagerMock.AssertNumberOfCalls(t, "GetAllKeys",1)
+		dbManagerMock.AssertCalled(t, "GetFromDB","1")
+		dbManagerMock.AssertNotCalled(t, "GetFromDB","2")
+		dbManagerMock.AssertNotCalled(t, "GetFromDB","3")
+		dbManagerMock.AssertNumberOfCalls(t, "GetFromDB",1) // 1 poniewaz jest return po pierwszej pustej wartosci
 		assert.Equal(t, http.StatusInternalServerError, recorder.Code)
-	})
 
+	})
 
 	t.Run("should return 500 code when is error during type assertion", func(t *testing.T) {
 
@@ -68,26 +75,60 @@ func TestDBGetAllHandler(t *testing.T) {
 		testSubject.DBGetAllHandler(recorder,req)
 		
 		//then
+		dbManagerMock.AssertNumberOfCalls(t, "GetAllKeys",1)
+		dbManagerMock.AssertCalled(t, "GetFromDB","1")
+		dbManagerMock.AssertNumberOfCalls(t, "GetFromDB",1)
 		assert.Equal(t,http.StatusInternalServerError,recorder.Code)
 	})
 
 	t.Run("should return JSON array compatible with given data ", func(t *testing.T) {
 
 		//given
-		expected := "[{\"url\":\"raccoon.com\",\"gcp\":\"image.png\",\"img\":\"image.png\"},{\"url\":\"raccoon.com\",\"gcp\":\"image2.png\",\"img\":\"image2.png\"}]"
+		expected := `[{` +
+			`"url":"raccoon.com",` +
+			`"gcp":"image.png",`+
+			`"img":"image.png"` +
+			`},`+
+			`{`+
+			`"url":"raccoon.com",`+
+			`"gcp":"image2.png",`+
+			`"img":"image2.png"`+
+			`}]`
+
 		req, err := http.NewRequest("GET", "v1/images", nil)
 		require.NoError(t, err)
 		recorder := httptest.NewRecorder()
 		dbManagerMock := mocks.DBManager{}
 		testSubject := NewHandler(&dbManagerMock)
 		dbManagerMock.On("GetAllKeys").Return([]string{"1","2"}, nil)
-		dbManagerMock.On("GetFromDB", "1").Return("{\"url\":\"raccoon.com\",\"gcp\":\"image.png\",\"img\":\"image.png\"}", nil)
-		dbManagerMock.On("GetFromDB", "2").Return("{\"url\":\"raccoon.com\",\"gcp\":\"image2.png\",\"img\":\"image2.png\"}", nil)
+
+		firstReturn := `
+			{
+				"url":"raccoon.com",
+				"gcp":"image.png",
+				"img":"image.png"
+			}`
+
+		secondReturn := `
+			{
+				"url":"raccoon.com",
+				"gcp":"image2.png",
+				"img":"image2.png"
+			}
+			`
+
+		dbManagerMock.On("GetFromDB", "1").Return(firstReturn, nil)
+		dbManagerMock.On("GetFromDB", "2").Return(secondReturn, nil)
 
 		//when
 		testSubject.DBGetAllHandler(recorder,req)
 
 		//then
+		dbManagerMock.AssertNumberOfCalls(t, "GetAllKeys",1)
+		dbManagerMock.AssertCalled(t, "GetFromDB","1")
+		dbManagerMock.AssertCalled(t, "GetFromDB","2")
+		dbManagerMock.AssertNumberOfCalls(t, "GetFromDB",2)
+		//dbManagerMock.AssertExpectations(t)
 		assert.Equal(t,expected,recorder.Body.String())
 	})
 }
