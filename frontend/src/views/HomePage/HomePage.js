1bda7ab9d9139318700d2524a24ecdb16ce94bdc
mwisnia97 <mwisnia97@gmail.com>
2021-08-06 15:52:26 +0200

    HomePage restore, deleted unnecessary function

--- frontend/src/views/HomePage/HomePage.js
+++ frontend/src/views/HomePage/HomePage.js
@@ -3,11 +3,8 @@ import Feed from 'views/HomePage/components/FeedArea/FeedArea';
 import Header from 'components/Header/Header';
 import Footer from 'components/Footer/Footer';
 import { Wrapper } from 'assets/styles/style';
-import { ImagesContext } from 'contexts/imagesContext';
-import React from 'react';
+
 const Home = () => {
-  const value = React.useContext(ImagesContext);
-  console.log(value);
   return (
     <>
       <Wrapper>
