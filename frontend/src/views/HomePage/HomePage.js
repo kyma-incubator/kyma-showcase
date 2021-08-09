502f22b8ef96ddb4919928ab2da7ca7013980f1f
Jakub Lyczko <jakub.lyczko1@gmail.com>
2021-08-09 14:49:39 +0200

    context moved to homepage

--- frontend/src/views/HomePage/HomePage.js
+++ frontend/src/views/HomePage/HomePage.js
@@ -3,17 +3,18 @@ import Feed from 'views/HomePage/components/FeedArea/FeedArea';
 import Header from 'components/Header/Header';
 import Footer from 'components/Footer/Footer';
 import { Wrapper } from 'assets/styles/style';
+import { ImagesContextProvider } from 'contexts/imagesContext';
 
 const Home = () => {
   return (
-    <>
+    <ImagesContextProvider>
       <Wrapper>
         <Header />
         <UploadImage />
         <Feed />
       </Wrapper>
       <Footer />
-    </>
+    </ImagesContextProvider>
   );
 };
 
