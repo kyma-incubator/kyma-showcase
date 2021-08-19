14ec00f0efc9ac24940d646021f77586a184d73e
mwisnia97 <mwisnia97@gmail.com>
2021-08-19 14:45:02 +0200

    Add loading state

--- frontend/src/views/HomePage/components/ImagesSection/ImagesSection.js
+++ frontend/src/views/HomePage/components/ImagesSection/ImagesSection.js
@@ -1,20 +1,25 @@
 import ImageTile from 'views/HomePage/components/ImageTile/ImageTile';
-import { UploadedImagesSection } from 'views/HomePage/components/ImagesSection/ImagesSection.styles';
+import { UploadedImagesSection, Loader } from 'views/HomePage/components/ImagesSection/ImagesSection.styles';
 import { useState, useContext, useEffect } from 'react';
 import React from 'react';
 import { ImagesContext } from 'contexts/imagesContext';
 
 const ImagesSection = () => {
   const [errorMessage, setErrorMessage] = useState('');
+  const [isLoading, setIsLoading] = useState('block');
 
   const { images, getImages } = useContext(ImagesContext);
+
   useEffect(() => {
     async function callAPI() {
+      setIsLoading('block');
       try {
         setErrorMessage('');
         await getImages();
+        setIsLoading('none');
       } catch (err) {
         setErrorMessage('Something went wrong');
+        setIsLoading('none');
       }
     }
     callAPI();
@@ -23,6 +28,9 @@ const ImagesSection = () => {
   return (
     <>
       <p>{errorMessage}</p>
+      <Loader>
+        <div className="loader" style={{ display: isLoading }}></div>
+      </Loader>
       <UploadedImagesSection>
         {images &&
           images.map(({ id, content }) => {
