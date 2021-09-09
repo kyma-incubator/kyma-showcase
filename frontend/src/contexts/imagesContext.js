aa8307ad29019fec26dec744eaa41ee721c36b03
Jakub Lyczko <jakub.lyczko1@gmail.com>
2021-09-09 11:24:34 +0200

    Sorting images in feed by date

--- frontend/src/contexts/imagesContext.js
+++ frontend/src/contexts/imagesContext.js
@@ -5,7 +5,12 @@ export const ImagesContext = React.createContext();
 export function ImagesContextProvider({ children }) {
   const [images, setImages] = useState([]);
 
-  const getImages = async () => setImages(await getAllImagesFromAPI());
+  const getImages = async () => {
+    let allImages = await getAllImagesFromAPI();
+    allImages = allImages.sort((d1, d2) => new Date(d1.date) - new Date(d2.date));
+    
+    setImages(allImages);
+  };
 
   return <ImagesContext.Provider value={{ images, getImages }}>{children}</ImagesContext.Provider>;
 }
