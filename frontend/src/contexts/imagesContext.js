243dc24f987ff03854d26e4ef191fdf7cf2ae187
Jakub Lyczko <lyczeq@gmail.com>
2021-09-13 14:44:45 +0200

    Sort images by date in feed (#109)
    * Sorting images in feed by date
    
    * Change naming, 'date' to 'time'
    
    * fix img context

--- frontend/src/contexts/imagesContext.js
+++ frontend/src/contexts/imagesContext.js
@@ -5,7 +5,11 @@ export const ImagesContext = React.createContext();
 export function ImagesContextProvider({ children }) {
   const [images, setImages] = useState([]);
 
-  const getImages = async () => setImages(await getAllImagesFromAPI());
+  const getImages = async () => {
+    let allImages = await getAllImagesFromAPI();
+    allImages = allImages?.sort((d1, d2) => new Date(d2.time) - new Date(d1.time));
+    setImages(allImages);
+  };
 
   return <ImagesContext.Provider value={{ images, getImages }}>{children}</ImagesContext.Provider>;
 }
