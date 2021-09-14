a63634cda569264b1388b26ad0468c7fbbd10971
Piotr Halama <piotr.halama@sap.com>
2021-09-14 09:18:42 +0200

    simplify

--- common_makefile.mk
+++ common_makefile.mk
@@ -1,7 +1,7 @@
 # Generic makefile - contains most used targets
 # Required variables:
 # IMG - name of the image, with docker push repository, full path and tag
-# IMG_DOCKER_TAG - name of the image, with docker push repository, full path and DOCKER_TAG to satisfy Protescan requirements
+# IMG_PR_TAG - name of the image, with docker push repository, full path and DOCKER_TAG to satisfy Protescan requirements
 # KMS_KEY_URL - required only when the target build is meant to be on post-submit or release job
 
 # Build the docker image
@@ -14,19 +14,14 @@ docker-build:
 docker-push:
 	docker push ${IMG}
 
-# Push additional tag to the existing image
-.PHONY: docker-push-pr
-docker-push-pr:
-	docker push ${IMG_DOCKER_TAG}
-
 # add second tag to the image
 .PHONY: ci-tag
 ci-tag:
-	docker tag ${IMG} ${IMG_DOCKER_TAG}
+	docker tag ${IMG} ${IMG_PR_TAG}
 
 # CI specified targets
 .PHONY: ci-pr
-ci-pr: docker-build ci-tag docker-push docker-push-pr
+ci-pr: docker-build docker-push
 
 .PHONY: ci-main
 ci-main: docker-build docker-push cosign
