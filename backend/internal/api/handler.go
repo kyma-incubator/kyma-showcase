e380b3fd0771debf9242630d1df98aac7b0c0d60
Hanna Gosciniak <hanngos10@gmail.com>
2021-09-16 15:45:08 +0200

    Deleting unneeded comments

--- backend/internal/api/handler.go
+++ backend/internal/api/handler.go
@@ -172,16 +172,6 @@ func (h Handler) GetAll(w http.ResponseWriter, r *http.Request) {
 }
 
 // getExtension returns extension of given by url image
-//func getExtension(bytes []byte) string {
-//	if bytes[0] == 0x89 && bytes[1] == 0x50 && bytes[2] == 0x4E && bytes[3] == 0x47 {
-//		return "png"
-//	} else if bytes[0] == 0xFF && bytes[1] == 0xD8 {
-//		return "jpg"
-//	} else if bytes[0] == 0x47 && bytes[1] == 0x49 && bytes[2] == 0x46 && bytes[3] == 0x38 {
-//		return "gif"
-//	}
-//	return ""
-//}
 func getExtension(bytes []byte) string {
 	mType := mimetype.Detect(bytes)
 	if mType.String() == "image/png" || mType.String() == "image/jpg" ||
