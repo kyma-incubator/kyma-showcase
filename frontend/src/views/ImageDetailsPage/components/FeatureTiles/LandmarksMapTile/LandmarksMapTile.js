a79a9349a111ff52e2d4d4f24e37a62931cad6ce
MrCherry <mwisnia97@gmail.com>
2021-09-20 13:12:48 +0200

    Displaying landmark map (#142)
    * Made map component, added fetching APIKEY from config
    
    * Added useEfect getting mapURL
    
    * Added API_KEY to frontend config
    
    * Remove unnecesary div and style
    
    * Clean code
    
    * Rename API_KEY and delete &key=
    
    * Rename API_KEY
    
    * Changed x, y getting

---
+++ frontend/src/views/ImageDetailsPage/components/FeatureTiles/LandmarksMapTile/LandmarksMapTile.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import { FeatureTemplate } from 'assets/styles/style';
+import { fetchMapAPIConfig } from 'API';
+
+export const LandmarksMapTile = ({ landmarks }) => {
+  const [mapUrl, setMapUrl] = useState('');
+
+  useEffect(() => {
+    const getMarks = async (landmarks) => {
+      let mapURL = 'https://maps.googleapis.com/maps/api/staticmap?&zoom=12&size=250x250&maptype=roadmap&markers=color:red%7Clabel:A%7C';
+      const mapKey = '&key=' + await fetchMapAPIConfig();
+      let x = landmarks[0].latitude;
+      let y = landmarks[0].longitude;
+      const finalUrl = mapURL + x + ',' + y + mapKey;
+      setMapUrl(finalUrl);
+    };
+    getMarks(landmarks);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <FeatureTemplate>
+        {mapUrl && <img alt="map" src={mapUrl}/>}
+    </FeatureTemplate>
+  );
+};
