3d860317c50fbce0760b0ad7e20008e03953447e
mwisnia97 <mwisnia97@gmail.com>
2021-09-20 11:20:52 +0200

    Added useEfect getting mapURL

--- frontend/src/views/ImageDetailsPage/components/FeatureTiles/LandmarksMapTile/LandmarksMapTile.js
+++ frontend/src/views/ImageDetailsPage/components/FeatureTiles/LandmarksMapTile/LandmarksMapTile.js
@@ -1,28 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FeatureTemplate } from 'assets/styles/style';
 import { fetchMapAPIConfig } from 'API';
 
-const getMarks = async (landmarks) => {
-  let mapURL = 'https://maps.googleapis.com/maps/api/staticmap?&zoom=12&size=250x250&maptype=roadmap&markers=color:red%7Clabel:A%7C';
-  const mapKey = await fetchMapAPIConfig();
-  let x = landmarks?.map((obj) => {
-    return (x = obj.latitude);
-  });
-  let y = landmarks?.map((obj) => {
-    return (y = obj.longitude);
-  });
-  const finalUrl = mapURL + x[0] + ',' + y[0] + mapKey;
-  console.log(finalUrl)
-  return finalUrl;
-};
-
 export const LandmarksMapTile = ({ landmarks }) => {
-  const map = getMarks(landmarks);
-  console.log(map);
+  const [mapUrl, setMapUrl] = useState('');
+
+  useEffect(() => {
+    const getMarks = async (landmarks) => {
+      let mapURL = 'https://maps.googleapis.com/maps/api/staticmap?&zoom=12&size=250x250&maptype=roadmap&markers=color:red%7Clabel:A%7C';
+      const mapKey = await fetchMapAPIConfig();
+      let x = landmarks?.map((obj) => {
+        return (x = obj.latitude);
+      });
+      let y = landmarks?.map((obj) => {
+        return (y = obj.longitude);
+      });
+      const finalUrl = mapURL + x[0] + ',' + y[0] + mapKey;
+      console.log(finalUrl);
+      setMapUrl(finalUrl);
+    };
+    getMarks(landmarks);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <FeatureTemplate>
       <div class="landmark-map">
-        {map && <img alt="map" src={map}></img>}
+        {mapUrl && <img alt="map" src={mapUrl}/>}
       </div>
     </FeatureTemplate>
   );
