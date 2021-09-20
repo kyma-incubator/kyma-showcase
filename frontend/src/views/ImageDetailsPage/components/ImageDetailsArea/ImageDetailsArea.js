6fae649f8ef7a4f37026f2edc7fb33e76b775f50
Jakub Lyczko <lyczeq@gmail.com>
2021-09-20 14:42:49 +0200

    NSFW filter  (#145)
    * Add nsfw filter and img styles based on it
    
    * Add nsfw function and subcription
    
    * Clean code and rename function
    
    * Add console log

--- frontend/src/views/ImageDetailsPage/components/ImageDetailsArea/ImageDetailsArea.js
+++ frontend/src/views/ImageDetailsPage/components/ImageDetailsArea/ImageDetailsArea.js
@@ -1,9 +1,16 @@
 import PropTypes from 'prop-types';
 import { ImageArea } from './ImageDetailsArea.styles.js';
 
-const ImageDetailsArea = ({ content }) => {
+const findNsfwTag = (gcp, key) => {
+  return gcp?.find((obj) => Object.keys(obj).includes(key));
+};
+
+const ImageDetailsArea = ({ content, gcp }) => {
+  gcp = gcp?.map(JSON.parse);
+  const nsfw = findNsfwTag(gcp, 'nsfw')?.nsfw;
   return (
-    <ImageArea>
+    <ImageArea nsfw={nsfw}>
+      {nsfw && <p>Nsfw</p>}
       <img src={content} alt="Uploaded content" />
     </ImageArea>
   );
@@ -11,6 +18,7 @@ const ImageDetailsArea = ({ content }) => {
 
 ImageDetailsArea.propTypes = {
   content: PropTypes.string.isRequired,
+  gcp: PropTypes.array.isRequired,
 };
 
 export default ImageDetailsArea;
