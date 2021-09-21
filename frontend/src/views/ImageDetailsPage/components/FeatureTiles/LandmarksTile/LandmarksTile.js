b58eb73fcecec464eb73a8671eed1f91e208a711
Jakub Lyczko <jakub.lyczko1@gmail.com>
2021-09-21 14:09:06 +0200

    Displaying coordinates in proper way

--- frontend/src/views/ImageDetailsPage/components/FeatureTiles/LandmarksTile/LandmarksTile.js
+++ frontend/src/views/ImageDetailsPage/components/FeatureTiles/LandmarksTile/LandmarksTile.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import { LandmarkTitle } from './LandmarksTile.styles';
 import { FeatureTemplate, FeatureItem, FeatureTitle } from 'assets/styles/style';
+import Carousel from 'react-elastic-carousel';
+
+const getCoordinates = (cords) => {
+  const latitudeDirection = cords.latitude > 0 ? 'N' : 'S';
+  const longitudeDirection = cords.longitude > 0 ? 'W' : 'E';
+
+  const latitudeValue = `${Math.abs(Number.parseFloat(cords.latitude).toFixed(5))} ${latitudeDirection}`;
+  const longitudeValue = `${Math.abs(Number.parseFloat(cords.longitude).toFixed(5))} ${longitudeDirection} `;
+
+  return (
+    <p>
+      {latitudeValue}, {longitudeValue}
+    </p>
+  );
+};
 
 export const LandmarksTile = ({ landmarks }) => {
   return (
     <FeatureTemplate>
       <FeatureTitle>Landmarks</FeatureTitle>
-      <ul>
+      <Carousel>
         {landmarks?.map((obj, i) => (
           <FeatureItem key={i}>
             <LandmarkTitle>{obj.name}</LandmarkTitle>
-            <p>Latitude: {obj.latitude}</p>
-            <p>Longitude: {obj.longitude}</p>
+            {getCoordinates(obj)}
           </FeatureItem>
         ))}
-      </ul>
+      </Carousel>
     </FeatureTemplate>
   );
 };
